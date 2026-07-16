cc60c8af6e3b9588918db92a080d6c37f0810eea
localhostamit <kumaramit02934@gmail.com>
2026-07-16 22:51:29 +0530

    fix 3

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <nav className="navbar">
-      <div className="logo">MyPortfolio</div>
+      <div className="logo">Amit kumar</div>
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
